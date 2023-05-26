b59d7ec3c205a35ac05f43690d06009bce509952
Renata Hassum <renatahassum@gmail.com>
2023-05-26 18:49:47 -0300

    User authenticated, validateSelfOrAdmin

---
+++ backend/src/main/java/com/naconsulta/naconsulta/services/AuthService.java
@@ -0,0 +1,37 @@
+package com.naconsulta.naconsulta.services;
+
+import com.naconsulta.naconsulta.entities.User;
+import com.naconsulta.naconsulta.repositories.UserRepository;
+import com.naconsulta.naconsulta.services.exceptions.ForbiddenException;
+import com.naconsulta.naconsulta.services.exceptions.UnauthorizedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AuthService {
+    @Autowired
+    private UserRepository userRepository;
+
+    //Pega o usuário logado
+    @Transactional(readOnly = true)
+    public User authenticated(){
+        try {
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            return userRepository.findByEmail(username);
+        }
+        catch (Exception e) {
+            throw new UnauthorizedException("Invalid user");
+        }
+    }
+
+    public void validateSelfOrAdmin(Long userId) {
+        User user = authenticated();
+//Se não for o mesmo usuário do id && Não for Admin lança exceção!
+        if(!user.getId().equals(userId) && !user.hasRole("ROLE_ADMIN")) {
+            throw new ForbiddenException("Access denied");
+        }
+    }
+}
+
