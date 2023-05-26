ce9933bb350eeebfb76c90eaeeb7bfaaa0af7574
Renata Hassum <renatahassum@gmail.com>
2023-05-26 15:17:16 -0300

    Adding custom exceptions(controllers/services), validation and security(OAuth/JWT)

--- backend/src/main/java/com/naconsulta/naconsulta/entities/User.java
+++ backend/src/main/java/com/naconsulta/naconsulta/entities/User.java
@@ -1,13 +1,17 @@
 package com.naconsulta.naconsulta.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements UserDetails,Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -27,7 +31,14 @@ public class User implements Serializable {
     @OneToMany(mappedBy = "user")
     private List<Consulta> consultas = new ArrayList<>();
 
-    public User() {}
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
 
     public User(Long id, String nome, String sobrenome, String sexo, String email, String password) {
         this.id = id;
@@ -93,4 +104,49 @@ public class User implements Serializable {
     public List<Consulta> getConsultas() {
         return consultas;
     }
+
+    public Set<Role> getRoles() { return roles; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
--- backend/src/main/java/com/naconsulta/naconsulta/repositories/UserRepository.java
+++ backend/src/main/java/com/naconsulta/naconsulta/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.naconsulta.naconsulta.repositories;
 
 import com.naconsulta.naconsulta.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    User findByEmail(String email);
 }
