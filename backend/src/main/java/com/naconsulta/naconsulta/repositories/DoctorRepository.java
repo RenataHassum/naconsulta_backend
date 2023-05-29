c0afa8ec8abc4439533684208dae4a4cb5119eef
Renata Hassum <renatahassum@gmail.com>
2023-05-29 17:22:33 -0300

    Doctor findAll, findById

--- backend/src/main/java/com/naconsulta/naconsulta/repositories/DoctorRepository.java
+++ backend/src/main/java/com/naconsulta/naconsulta/repositories/DoctorRepository.java
@@ -2,6 +2,17 @@ package com.naconsulta.naconsulta.repositories;
 
 import com.naconsulta.naconsulta.entities.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+//    @Query("SELECT obj FROM Doctor obj " +
+//            "WHERE UPPER(obj.firstName) LIKE UPPER(CONCAT('%', :name, '%')) UPPER(CONCAT('%', :last, '%')) ")
+//    List<Doctor> searchByName(String name, String last);
+
+    @Query("SELECT obj FROM Doctor obj " +
+            "WHERE UPPER(obj.firstName) LIKE UPPER(CONCAT('%', :name, '%')) ")
+    List<Doctor> searchByName(String name);
+
 }
